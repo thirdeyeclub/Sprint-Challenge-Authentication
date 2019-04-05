be31a1b91442562b6b4ceb45fcc92e40cd9ea2c8
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-05 12:04:31 -0400

    built login

--- config/routes.js
+++ config/routes.js
@@ -10,7 +10,7 @@ module.exports = server => {
 
 function register(req, res) {
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10)
+  const hash = bcrypt.hashSync(user.password, 4)
   user.password = hash;
   add(user).then(add=>{
     res.status(201).json(add);
@@ -21,7 +21,26 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
-
+  let {username, password} = req.body;
+  findBy({username}).then(
+    user=>{
+      if(user && bcrypt.compareSync(password, user.password)){
+        const payload = {
+          subject: user.id,
+          username: user.username
+        }
+        const options = {
+          expiresIn: '1d'
+        }
+        const token = jwt.sign(payload, jwt, options)
+        res.status(200).json({message: `Hello ${user.username}.`, token});
+      } else{
+        res.status(401).json({message: 'No Pass'});
+      }
+    }
+  ).catch(err=>{
+    res.status(500).json(err);
+  })
 }
 
 function getJokes(req, res) {
@@ -42,4 +61,8 @@ function getJokes(req, res) {
 async function add(user) {
   const [id] = await db('users').insert(user)
   return db('users').where({id}).first()
+}
+
+function findBy(filter) {
+  return db('users').where(filter).first()
 }
